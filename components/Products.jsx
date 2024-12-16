912e2acc42dd68384de9087c6c90f88370d98065
satish <satishwillowood@gmail.com>
2024-12-16 18:31:05 +0530

    filter functionality minimum added

--- components/Products.jsx
+++ components/Products.jsx
@@ -1,18 +1,67 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ProductList from "./ProductList";
+import { RiArrowDropDownLine } from "react-icons/ri";
 const Products = () => {
+  const [isOpen, setOpen] = useState(null);
+
+  const ListArrays = [
+    { title: "Brands" },
+    { title: "Category" },
+    { title: "Pre Or Post Emergence" },
+    { title: "Selective on Non" },
+    { title: "Cerfified Or Research" },
+    { title: "Hybrid Or Op" },
+    { title: "Target Pest" },
+    { title: "Formulation" }
+  ];
+
+  const DropDownInbox = [
+    { list: "Bayer" },
+    { list: "Green Life" },
+    { list: "Nacl" },
+    { list: "Bharat Certis" },
+    { list: "IFFCO Mc" },
+    { list: "Crystal" }
+  ];
+
   return (
     <>
       <section className="relative flex border-t border-solid border-border-200 border-opacity-70 md:flex-col lg:flex-row">
         <div className="hidden lg:block xl:block text-black ">
-          <aside className="sidefiler-wrapper h-screen bg-light lg:sticky  lg:top-20 lg:block lg:w-72 xl:block xl:w-72">
-            Filterrrr
+          <aside className="sidefiler-wrapper h-screen bg-light lg:sticky bg-white  lg:top-24 lg:block lg:w-72 xl:block xl:w-72">
+            <div className="mx-4">
+              <h2 className="text-cent font-semibold leading-5 py-4 text-orange-900">Rest Filter</h2>
+              <div className="brands-acco ">
+                <div className="flex items-center flex-col justify-between my-3">
+                  {ListArrays.map((item, index) => (
+                    <>
+                      <div className="flex items-center justify-between w-full my-3 cursor-pointer">
+                        <h2 className="text-[1.1rem] ">{item.title}</h2>
+                        <RiArrowDropDownLine size={28}></RiArrowDropDownLine>
+                      </div>
+                      <div className="flex flex-col">
+                        {DropDownInbox.map((list) => (
+                          <>
+                            {isOpen === index && (
+                              <label htmlFor="">
+                                <input type="checkbox" className="mr-2" />
+                                <span className="text-sm">{list.list}</span>
+                              </label>
+                            )}
+                          </>
+                        ))}
+                      </div>
+                    </>
+                  ))}
+                </div>
+              </div>
+            </div>
           </aside>
-        <div className=" text-black sticky top-14 z-10 flex h-14 items-center justify-between border-b border-t border-border-200 bg-light px-5 py-3 md:top-16 md:h-16 lg:top-22 lg:hidden lg:px-7 xl:hidden">
-          <button className="flex h-8 items-center rounded border border-border-200 bg-gray-100 bg-opacity-90 px-3 py-1 text-sm font-semibold text-heading transition-colors duration-200 hover:border-accent-hover hover:bg-accent hover:text-light focus:border-accent-hover focus:bg-accent focus:text-light focus:outline-0 md:h-10 md:px-4 md:py-1.5 md:text-base">
-            Filter
-          </button>
-        </div>
+          <div className=" text-black sticky top-14 z-10 flex h-14 items-center justify-between border-b border-t border-border-200 bg-light px-5 py-3 md:top-16 md:h-16 lg:top-22 lg:hidden lg:px-7 xl:hidden">
+            <button className="flex h-8 items-center rounded border border-border-200 bg-gray-100 bg-opacity-90 px-3 py-1 text-sm font-semibold text-heading transition-colors duration-200 hover:border-accent-hover hover:bg-accent hover:text-light focus:border-accent-hover focus:bg-accent focus:text-light focus:outline-0 md:h-10 md:px-4 md:py-1.5 md:text-base">
+              Filter
+            </button>
+          </div>
         </div>
         <ProductList></ProductList>
       </section>
